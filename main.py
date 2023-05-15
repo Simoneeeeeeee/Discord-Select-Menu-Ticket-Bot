af4718931bfbd15af77927164e60b44bff95bc02
Simone <100680246+Simoneeeeeeee@users.noreply.github.com>
2023-05-15 20:39:16 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-#Version: 1.4
+#Version: 1.5
 #GitHub: https://github.com/Simoneeeeeeee/Discord-Select-Menu-Ticket-Bot
-#Discord: Simone#0782
-#This Bot is using Py-Cord as Discord API Wrapper
-
+#Discord: discord.gg/ycZDpat7dB
 
 import discord
 import asyncio
@@ -15,8 +13,9 @@ from pytz import timezone
 with open("config.json", mode="r") as config_file:
     config = json.load(config_file)
 
-BOT_TOKEN = config["token"]
-BOT_PREFIX = config["prefix"]
+BOT_TOKEN = config["token"]  #Bot Token create bot on discord.dev
+BOT_PREFIX = config["prefix"] #Prefix for the Commands if you dont use Slash Commands
+TIMEZONE = config["timezone"] #Timezone for the Log you find all timezones at https://gist.github.com/heyalexej/8bf688fd67d7199be4a1682b3eec7568
 
 GUILD_ID = config["guild_id"] #Server ID
 TICKET_CHANNEL = config["ticket_channel_id"] #Where the bot should send the Embed + SelectMenu
@@ -46,14 +45,14 @@ class MyView(discord.ui.View):
         placeholder="Choose a Ticket option",
         options=[
             discord.SelectOption(
-                label="Support1",
-                emoji="❓",
-                value="support1"
+                label="Support1",  #Name of the 1 Option
+                emoji="❓",        # Emoji of the 1 Option
+                value="support1"   #Don't change this value!
             ),
             discord.SelectOption(
-                label="Support2",
-                emoji="❓",
-                value="support2"
+                label="Support2",  #Name of the 2 Option
+                emoji="❓",        # Emoji of the 2 Option
+                value="support2"   #Don't change this value!
             )
         ]
     )
@@ -71,7 +70,7 @@ class MyView(discord.ui.View):
                         return
                 category = bot.get_channel(CATEGORY_ID1)
                 ticket_channel = await guild.create_text_channel(f"ticket-{interaction.user.id}", category=category,
-                                                                topic=f"Ticket from {interaction.user} \nUser-ID: {interaction.user.id}")
+                                                                topic=f"{interaction.user.id}")
 
                 await ticket_channel.set_permissions(guild.get_role(TEAM_ROLE1), send_messages=True, read_messages=True, add_reactions=False,
                                                     embed_links=True, attach_files=True, read_message_history=True,
@@ -105,7 +104,7 @@ class MyView(discord.ui.View):
                         return 
                 category = bot.get_channel(CATEGORY_ID2)
                 ticket_channel = await guild.create_text_channel(f"ticket-{interaction.user.id}", category=category,
-                                                                    topic=f"Ticket from {interaction.user} \nUser-ID: {interaction.user.id}")
+                                                                    topic=f"{interaction.user.id}")
                 await ticket_channel.set_permissions(guild.get_role(TEAM_ROLE2), send_messages=True, read_messages=True, add_reactions=False,
                                                         embed_links=True, attach_files=True, read_message_history=True,
                                                         external_emojis=True)
@@ -138,13 +137,13 @@ class delete(discord.ui.View):
         fileName = f"{interaction.channel.name}.txt"
         with open(fileName, "w", encoding='utf-8') as file:
             async for msg in interaction.channel.history(limit=None, oldest_first=True):
-                time = msg.created_at.replace(tzinfo=timezone('UTC')).astimezone(timezone('Europe/Berlin'))
+                time = msg.created_at.replace(tzinfo=timezone('UTC')).astimezone(timezone(TIMEZONE))
                 file.write(f"{time} - {msg.author.display_name}: {msg.clean_content}\n")
 
         embed = discord.Embed(
                 description=f'Ticket is closing in 5Sec.',
                 color=0xff0000)
-        embed2 = discord.Embed(title="Ticket Closed!", description=f"Ticket-Name: {interaction.channel.name}\n Closed-From: {interaction.user.name}\n Transcript: ", color=discord.colour.Color.blue())
+        embed2 = discord.Embed(title="Ticket Closed!", description=f"Ticket Name: {interaction.channel.name}\n Ticket from: <@{interaction.channel.topic}>\n Closed from: {interaction.user.name}\n Transcript: ", color=discord.colour.Color.blue())
         file = discord.File(fileName)
         await interaction.response.send_message(embed=embed)
         await channel.send(embed=embed2)
@@ -159,5 +158,53 @@ async def ticket(ctx):
     channel = bot.get_channel(TICKET_CHANNEL)
     embed = discord.Embed(title="Support-Tickets", color=discord.colour.Color.blue())
     await channel.send(embed=embed, view=MyView())
+
+@bot.command()
+async def close(ctx):
+    channel = bot.get_channel(LOG_CHANNEL)
+
+    fileName = f"{ctx.channel.name}.txt"
+    with open(fileName, "w", encoding='utf-8') as file:
+        async for msg in ctx.channel.history(limit=None, oldest_first=True):
+            time = msg.created_at.replace(tzinfo=timezone('UTC')).astimezone(timezone(f"{TIMEZONE}"))
+            file.write(f"{time} - {msg.author.display_name}: {msg.clean_content}\n")
+
+    embed = discord.Embed(description=f'Ticket is closing in 5Sec.', color=0xff0000)
+    embed2 = discord.Embed(title="Ticket Closed!", description=f"Ticket Name: {ctx.channel.name}\n Ticket from: <@{ctx.channel.topic}>\n Closed from: {ctx.author.name}\n Transcript: ", color=discord.colour.Color.blue())
+    file = discord.File(fileName)
+    await ctx.reply(embed=embed)
+    await channel.send(embed=embed2)
+    await asyncio.sleep(1)
+    await channel.send(file=file)
+    await asyncio.sleep(5)
+    await ctx.channel.delete(reason="Ticket closed by user")
+
+@bot.command()
+async def add(ctx, member: discord.Member):
+    category1 = bot.get_channel(CATEGORY_ID1)
+    category2 = bot.get_channel(CATEGORY_ID2)
+    if ctx.channel.category_id == CATEGORY_ID1 or ctx.channel.category_id == CATEGORY_ID2:
+        await ctx.channel.set_permissions(member, send_messages=True, read_messages=True, add_reactions=False,
+                                            embed_links=True, attach_files=True, read_message_history=True,
+                                            external_emojis=True)
+        embed = discord.Embed(description=f'Added {member.mention} to this Ticket <#{ctx.channel.id}>! \n Use !remove @User or ID to remove a User.', color=discord.colour.Color.green())
+        await ctx.reply(embed=embed)
+    else:
+        embed = discord.Embed(description=f'You can only use this command in a Ticket', color=discord.colour.Color.red())
+        await ctx.reply(embed=embed)
+
+@bot.command()
+async def remove(ctx, member: discord.Member):
+    category1 = bot.get_channel(CATEGORY_ID1)
+    category2 = bot.get_channel(CATEGORY_ID2)
+    if ctx.channel.category_id == CATEGORY_ID1 or ctx.channel.category_id == CATEGORY_ID2:
+        await ctx.channel.set_permissions(member, send_messages=False, read_messages=False, add_reactions=False,
+                                            embed_links=False, attach_files=False, read_message_history=False,
+                                            external_emojis=False)
+        embed = discord.Embed(description=f'Removed {member.mention} from this Ticket <#{ctx.channel.id}>! \n Use !add @User or ID to add a User.', color=discord.colour.Color.green())
+        await ctx.reply(embed=embed)
+    else:
+        embed = discord.Embed(description=f'You can only use this command in a Ticket', color=discord.colour.Color.red())
+        await ctx.reply(embed=embed)
 
 bot.run(BOT_TOKEN)
